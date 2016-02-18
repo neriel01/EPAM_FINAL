d803e7523ab447d0c4315343b40d9df260cbdb99
neriel01 <lolco2107>
2016-02-18 08:36:46 +0200

    almost done, testing

--- js/slider.js
+++ js/slider.js
@@ -3,10 +3,7 @@
  */
 var arrow_lft = document.getElementById("arrow-lft");
 var arrow_rht = document.getElementById("arrow-rght");
-var images_container = document.getElementById("images-container");
 var element = document.getElementById("left-element");
-var device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width; //we need another margin in responsive
-//layout
 var margin_step = 210;
 var initial_margin = 210;
 var min = 210;
